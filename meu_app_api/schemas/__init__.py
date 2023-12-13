4ef81bc36b244e395a3cb841ebaeff8236f29a91
Tiago Peniche <140699897+Penichezito@users.noreply.github.com>
2023-12-12 22:18:44 -0300

    Delete meu_app_api/schemas/__init__.py

--- meu_app_api/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-from schemas.comentario import ComentarioSchema
-from schemas.jogo import GameSchema, GameBuscaSchema, GameViewSchema, \
-                         ListagemGamesSchema, GameDelSchema, apresenta_jogos, \
-                         apresenta_jogo, apresenta_jogos
-from schemas.error import ErrorSchema
